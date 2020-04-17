df47e5cac582add50d10ec29abec4a89c94166be
Visalakshi Nutulapati <visalakshi@Visalakshis-Air.lan>
2020-04-17 13:29:12 -0400

    Created a new build-jar.sh file which has the code to create a jar file that will launch the applicaton

---
+++ README.txt
@@ -0,0 +1,3 @@
+Step 1: clone the project from github.
+Step 2: cd into the project folder.
+Step 3: run the command - build-jar.sh  (e.g. sh build-jar.sh, or ./build-jar.sh)
--- build-jar.sh
+++ build-jar.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/sh 
 javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1-3.jar -d build/ src/java/*.java
 javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1-3.jar:build/ -d build/ test/java/*.java
 java -cp .:lib/junit-4.12:lib/hamcrest-core-1.3.jar:build/ org.junit.runner.JUnitCore EdgeConnectorTest
-java -cp build/ RunEdgeConvert
+
+#Shell script to create jar file - Deployment Exercise
+jar cvmf build/tmp/jar/MANIFEST.MF  EdgeConvertGUI.jar  ./lib/*.jar -C ./build .
+java -jar  EdgeConvertGUI.jar
